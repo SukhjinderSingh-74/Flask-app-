c70b1489cf7d01e70e5378131d9557a13ef6eac9
SukhjinderSingh-74 <156149972+SukhjinderSingh-74@users.noreply.github.com>
2024-10-11 15:29:25 -0400

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Upload from './Upload';
+
+const App = () => {
+    return (
+        <div>
+            <h1>File and Text Upload</h1>
+            <Upload />
+        </div>
+    );
+};
+
+export default App;
---
+++ frontend/src/Upload.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Upload = () => {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!file) {
+            alert('Please select a file before submitting.');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const response = await axios.post("http://34.75.79.119:5000/upload", formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            alert(response.data.message);
+        } catch (error) {
+            console.error('Error uploading file:', error.response ? error.response.data : error.message);
+            alert('Error uploading file.');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="file" onChange={handleFileChange} required />
+            <button type="submit">Upload</button>
+        </form>
+    );
+};
+
+export default Upload;
